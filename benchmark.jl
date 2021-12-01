8b939055e1913bb010447d0423f3da1e7f77a5ec
Jakob Sachs <jakobsachs1999@gmail.com>
2021-12-01 12:41:53 +0100

    Changed benchmark.jl output

--- benchmark.jl
+++ benchmark.jl
@@ -5,4 +5,6 @@ include("./advent-1.jl")
 using .Day1  
 benchmark_d1 = @benchmarkable Day1.main()
 tune!(benchmark_d1)
-median(run(benchmark_d1))
+println("Benchmark: \n")
+display(median(run(benchmark_d1)))
+println("\ndone!")
