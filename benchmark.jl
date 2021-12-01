eb206c508b32f156bfbc80d4536d2252ca11cbd2
Jakob Sachs <jakobsachs1999@gmail.com>
2021-12-01 12:05:42 +0100

    added benchmark script for days

---
+++ benchmark.jl
@@ -0,0 +1,8 @@
+using BenchmarkTools, Random, ProfileView, Profile
+
+# benchmark day 1
+include("./advent-1.jl")
+using .Day1  
+benchmark_d1 = @benchmarkable Day1.main()
+tune!(benchmark_d1)
+median(run(benchmark_d1))
