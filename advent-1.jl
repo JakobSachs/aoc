07967a82ef21a35bc6943315b9934f6722c85669
Jakob Sachs <jakobsachs1999@gmail.com>
2021-12-01 12:05:32 +0100

    Made day1 into a module

--- advent-1.jl
+++ advent-1.jl
@@ -1,23 +1,33 @@
-function count_increases(arr::Vector{Int32})
-  count::UInt32 = 0
+module Day1
 
-  for i in 2:length(arr)
-    if arr[i] > arr[i-1]
-      count += 1
+  function count_increases(arr::Vector)
+    count::UInt32 = 0
+
+    for i in 2:length(arr)
+      if arr[i] > arr[i-1]
+        count += 1
+      end
     end
+
+    return count
   end
 
-  return count
+  global inc_measurement = 0
+  global inc_sum = 0
+
+  function main()
+    measurements = parse.(Int16,readlines("input_day1.txt"))
+    global inc_measurement = count_increases(measurements)
+  
+    sums = [sum(measurements[i:i+2]) for i in 1:length(measurements)-2]
+    global inc_sum = count_increases(sums)
+  end
+
+  function output()
+    println("$inc_measurement measurements are larger then the previous measurement")
+    println("$inc_sum sums are larger then the previous sum")
+  end
 end
 
-measurements = parse.(Int32,readlines("input_day1.txt"))
-inc_measurement = count_increases(measurements)
-
-println("$inc_measurement measurements are larger then the previous measurement")
-
-sums = [sum(measurements[i:i+2]) for i in 1:length(measurements)-2]
-inc_sum = count_increases(sums)
-
-println("$inc_sum sums are larger then the previous sum")
-
-
+Day1.main()
+Day1.output()
