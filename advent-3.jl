d5046ff4bba5607d65a2ea97babee5c3777c9b83
Jakob Sachs <jakobsachs1999@gmail.com>
2021-12-03 17:20:35 +0100

    Finished day 3 and adapted benchmark script

---
+++ advent-3.jl
@@ -0,0 +1,81 @@
+module Day3
+
+global γ, ϵ
+global oxygen_rate, CO2_rate
+
+function count_bit(lines, bit)
+  count_1 = count_0 = 0
+  for l in lines
+    if l[bit] == '1'
+      count_1 += 1
+    elseif l[bit] == '0'
+      count_0 += 1
+    end
+  end
+  return (count_0, count_1)
+end
+
+function main()
+
+  lines = readlines("input_day3.txt")
+  bits = length(lines[1])
+
+  γ_tmp = ""
+  ϵ_tmp = ""
+
+  for i = 1:bits
+    res = count_bit(lines, i)
+    if res[1] <=res[2]
+      γ_tmp *= "1"
+      ϵ_tmp *= "0"
+    else
+      γ_tmp *= "0"
+      ϵ_tmp *= "1"
+    end
+  end
+
+  global γ = parse(Int, γ_tmp, base = 2)
+  global ϵ = parse(Int, ϵ_tmp, base = 2)
+
+  # calculate 'the oxygen rate'
+  lines_tmp = deepcopy(lines)
+  bit = 1
+
+  while length(lines_tmp) > 1
+    res = count_bit(lines_tmp, bit)
+
+    if res[1] <= res[2]
+      filter!(l->l[bit] == '1', lines_tmp)
+    else
+      filter!(l->l[bit] == '0', lines_tmp)
+    end
+    bit += 1
+  end
+
+  global oxygen_rate = parse(Int, first(lines_tmp),base=2)
+  
+  lines_tmp = deepcopy(lines)
+  bit = 1
+
+  while length(lines_tmp) > 1
+    res = count_bit(lines_tmp, bit)
+
+    if res[1] <= res[2]
+      filter!(l->l[bit] == '0', lines_tmp)
+    else
+      filter!(l->l[bit] == '1', lines_tmp)
+    end
+    bit += 1
+  end
+
+  global CO2_rate = parse(Int, first(lines_tmp),base=2)
+end
+
+function output()
+  println("γ:\t$γ\tϵ:\t$ϵ\tPower:\t$(γ*ϵ)")
+  println("O2:\t$oxygen_rate\tCO2:\t$CO2_rate\tScore:\t$((oxygen_rate * CO2_rate))")
+end
+end
+
+Day3.main()
+Day3.output()
--- benchmark.jl
+++ benchmark.jl
@@ -1,19 +1,35 @@
 using BenchmarkTools
 
-# benchmark day 1
-include("./advent-1.jl")
-using .Day1  
-benchmark_d1 = @benchmarkable Day1.main()
-tune!(benchmark_d1)
-println("Benchmark: \n")
-display(median(run(benchmark_d1)))
-println("\ndone!")
+for arg in Base.ARGS
+  
+  if arg == "1"
+    # benchmark day 1
+    include("./advent-1.jl")
+    using .Day1  
+    benchmark_d1 = @benchmarkable Day1.main()
+    tune!(benchmark_d1)
+    println("Benchmark: \n")
+    display(median(run(benchmark_d1)))
+    println("\ndone!")
+  elseif arg == "2"    
+    # benchmark day 2
+    include("./advent-2.jl")
+    using .Day2
+    benchmark_d2 = @benchmarkable Day2.main()
+    tune!(benchmark_d2)
+    println("Benchmark: \n")
+    display(median(run(benchmark_d2)))
+    println("\ndone!")
+  elseif arg == "3"
+    # benchmark day 3
+    include("./advent-3.jl")
+    using .Day3
+    benchmark_d3 = @benchmarkable Day3.main()
+    tune!(benchmark_d3)
+    println("Benchmark: \n")
+    display(median(run(benchmark_d3)))
+    println("\ndone!")
+  end
+end
 
-# benchmark day 2
-include("./advent-2.jl")
-using .Day2
-benchmark_d2 = @benchmarkable Day2.main()
-tune!(benchmark_d2)
-println("Benchmark: \n")
-display(median(run(benchmark_d2)))
-println("\ndone!")
+
