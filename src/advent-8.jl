23c556b76e2335f6381a01b04e3499b4802cce39
Jakob Sachs <jakobsachs1999@gmail.com>
2021-12-09 10:18:44 +0100

    Found a very bad (but working) solutin for day 8

--- src/advent-8.jl
+++ src/advent-8.jl
@@ -1,6 +1,7 @@
 module Day8
 
 global counts = []
+global output_sum = 0
 
 function main()
   lines = readlines(dirname(@__DIR__) * "/data/input_day8.txt")
@@ -31,10 +32,85 @@ function main()
   end
 
   global counts = sum(counters)
+
+  sum_local = 0
+
+  for (l, l_out) in zip(inputs, outputs)
+
+    values_dict = Dict{Int,String}()
+
+    for n in split(l, ' ')
+      if length(n) == 2
+        values_dict[1] = n
+      elseif length(n) == 4
+        values_dict[4] = n
+      elseif length(n) == 3
+        values_dict[7] = n
+      elseif length(n) == 7
+        values_dict[8] = n
+      end
+    end
+
+
+
+    for n in split(l, ' ')
+      if length(n) == 5 && length(intersect(n, values_dict[4])) == 2
+        values_dict[2] = n
+      end
+    end
+
+    for n in split(l, ' ')
+      if length(n) == 5 && length(intersect(n, values_dict[2])) == 4
+        values_dict[3] = n
+      end
+    end
+
+    for n in split(l, ' ')
+      if length(n) == 5 && (length(setdiff(n, values_dict[3])) == 1) && (n ∉ values(values_dict))
+        values_dict[5] = n
+      end
+    end
+
+
+    for n in split(l, ' ')
+      if length(n) == 6 && length(intersect(n, values_dict[4])) == 4
+        values_dict[9] = n
+      end
+    end
+
+    for n in split(l, ' ')
+      if length(n) == 6 && length(intersect(n, values_dict[4])) == 3 && length(intersect(n, values_dict[1])) == 1
+        values_dict[6] = n
+      end
+    end
+
+    for n in split(l, ' ')
+      if length(n) == 6 && length(intersect(n, values_dict[4])) == 3 && length(intersect(n, values_dict[1])) == 2
+        values_dict[0] = n
+      end
+    end
+
+
+    output_code = ""
+
+    for n in split(l_out, ' ')
+      for i = 0:9
+        if Set(n) == Set(values_dict[i])
+          output_code *= string(i)
+        end
+      end
+    end
+
+    sum_local += parse(Int, output_code)
+
+  end
+
+  global output_sum = sum_local
 end
 
 function output()
   println("amount of 1,4,7,8:\t$counts")
+  println("sum of outputs:\t\t$output_sum")
 end
 end
 
