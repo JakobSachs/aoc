8d0614b9737327c7acb0218b4e3832d757051183
Jakob Sachs <jakobsachs1999@gmail.com>
2021-12-13 15:33:07 +0100

    Rewrote benchmark.jl to auto-create the table

--- README.md
+++ README.md
@@ -4,21 +4,6 @@ My solutions for the [advent of code](https://adventofcode.com/2021) 2021 🎄�
 Anyone that wants to  can join this private leaderboard : `1597512-c0dd3e93`
 
 ## Performance
-| Day 📅| Time⏳ | Memory 💾 |
-| --- | ---- | ------ |
-|  1  | 514.300 μs | 334.08 KiB | 
-|  2  | 256.800 μs | 76.12 KiB  | 
-|  3  | 662.802 μs | 325.97 KiB | 
-|  4  | 13.169 ms  | 14.66 MiB  |
-|  5  | 3.667 ms   | 5.75 MiB   |
-|  6  | 46.294 μs  | 78.25 KiB  |
-|  7  | 32.093 ms  | 15.26 MiB  |
-|  8  | 12.000 ms  | 13.30 MiB  |
-|  9  | 7.411 ms   | 3.07 MiB   |
-|  10 | 382.832 μs | 997.12 KiB |
-|  11 | 1.101 ms   | 1.46 MiB   |
-|  12 |    -       |    -       |
-|  12 | 870.696 μs | 762.67 KiB |
-
-
 (Measured using `BenchmarkTools.@benchmark`)
+
+[Benchmarks][benchmarks.md]
--- benchmark.jl
+++ benchmark.jl
@@ -1,133 +1,109 @@
-using BenchmarkTools
+using BenchmarkTools, Printf, Suppressor, DataStructures
 
-for arg in Base.ARGS
+"Returns the given time (in nanoseconds) in a nice format"
+function format_time(t::Float64)::String
+  unit = ""
+  if t > 1e9
+    # print in seconds
+    t /= 1e9
+  elseif t > 1e6
+    # print in milliseconds
+    t /= 1e6
+    unit = "m"
+  elseif t > 1e3
+    # print in microseconds
+    t /= 1e3
+    unit = "µ"
+  else
+    #print in nanoseconds\
+    unit = "n"
+  end
 
-  if arg == "1"
-    # benchmark day 1
-    include("./src/advent-1.jl")
-    using .Day1
-    benchmark_d1 = @benchmarkable Day1.main()
-    tune!(benchmark_d1)
-    println("Benchmark: \n")
-    display(median(run(benchmark_d1)))
-    println("\ndone!")
-  elseif arg == "2"
-    # benchmark day 2
-    include("./src/advent-2.jl")
-    using .Day2
-    benchmark_d2 = @benchmarkable Day2.main()
-    tune!(benchmark_d2)
-    println("Benchmark: \n")
-    display(median(run(benchmark_d2)))
-    println("\ndone!")
-  elseif arg == "3"
-    # benchmark day 3
-    include("./src/advent-3.jl")
-    using .Day3
-    benchmark_d3 = @benchmarkable Day3.main()
-    tune!(benchmark_d3)
-    println("Benchmark: \n")
-    display(median(run(benchmark_d3)))
-    println("\ndone!")
-  elseif arg == "4"
-    # benchmark day 4
-    include("./src/advent-4.jl")
-    using .Day4
-    benchmark_d4 = @benchmarkable Day4.main()
-    tune!(benchmark_d4)
-    println("Benchmark: \n")
-    display(median(run(benchmark_d4)))
-    println("\ndone!")
-  elseif arg == "5"
-    # benchmark day 5
-    include("./src/advent-5.jl")
-    using .Day5
-    benchmark_d5 = @benchmarkable Day5.main()
-    tune!(benchmark_d5)
-    println("Benchmark: \n")
-    display(median(run(benchmark_d5)))
-    println("\ndone!")
-  elseif arg == "6"
-    # benchmark day 6
-    include("./src/advent-6.jl")
-    using .Day6
-    benchmark_d6 = @benchmarkable Day6.main()
-    tune!(benchmark_d6)
-    println("Benchmark: \n")
-    display(median(run(benchmark_d6)))
-    println("\ndone!")
-  elseif arg == "7"
-    # benchmark day 7
-    include("./src/advent-7.jl")
-    using .Day7
-    benchmark_d7 = @benchmarkable Day7.main()
-    tune!(benchmark_d7)
-    println("Benchmark: \n")
-    display(median(run(benchmark_d7)))
-    println("\ndone!")
-  elseif arg == "8"
-    # benchmark day 78
-    include("./src/advent-8.jl")
-    using .Day8
-    benchmark = @benchmarkable Day8.main()
-    tune!(benchmark)
-    println("Benchmark: \n")
-    display(median(run(benchmark)))
-    println("\ndone!")
-  elseif arg == "9"
-    # benchmark day 9
-    include("./src/advent-9.jl")
-    using .Day9
-    benchmark = @benchmarkable Day9.main()
-    tune!(benchmark)
-    println("Benchmark: \n")
-    display(median(run(benchmark)))
-    println("\ndone!")
-  elseif arg == "10"
-    # benchmark day 10
-    include("./src/advent-10.jl")
-    using .Day10
-    benchmark = @benchmarkable Day10.main()
-    tune!(benchmark)
-    println("Benchmark: \n")
-    display(median(run(benchmark)))
-    println("\ndone!")
-  elseif arg == "11"
-    # benchmark day 11
-    include("./src/advent-11.jl")
-    using .Day11
-    benchmark = @benchmarkable Day11.main()
-    tune!(benchmark)
-    println("Benchmark: \n")
-    display(median(run(benchmark)))
-    println("\ndone!")
-  elseif arg == "12"
-    # benchmark day 11
-    try
-      include("./src/advent-12.jl")
-      using .Day12
-      benchmark = @benchmarkable Day12.main()
-      tune!(benchmark)
-      println("Benchmark: \n")
-      display(median(run(benchmark)))
-      println("\ndone!")
-    catch e
+  res = @sprintf "%.3f " t
+  res *= unit * "s"
+  return res
+end
+
+"Returns the given memory-amount (in bytes) in a nice format"
+function format_mem(b)::String
+  unit = ""
+  if b > 1024^3
+    # print in GibiBytes
+    b /= 1024^3
+    unit = "Gi"
+  elseif b > 1024^2
+    # print in MebiBytes
+    b /= 1024^2
+    unit = "Mi"
+  elseif b > 1024
+    # print in KibiBytes
+    b /= 1e3
+    unit = "Ki"
+  end
+
+  res = @sprintf "%.3f " b
+  res *= unit * "B"
+  return res
+end
+
+files = readdir(dirname(@__DIR__) * "/aoc21/src")
+
+if length(ARGS) == 0
+  #run all benchmarks and output to benchmarks.md
+
+  outputs = SortedDict{Int,String}()
+
+  for f in files
+    @suppress begin
+      include(dirname(@__DIR__) * "/aoc21/src/" * f)
+      using .Day
     end
-  elseif arg == "13"
-    # benchmark day 11
-    try
-      include("./src/advent-13.jl")
-      using .Day13
-      benchmark = @benchmarkable Day13.main()
-      tune!(benchmark)
-      println("Benchmark: \n")
-      display(median(run(benchmark)))
-      println("\ndone!")
-    catch e
+    benchmark = @benchmarkable Day.main()
+    tune!(benchmark)
+
+    res = median(run(benchmark))
+
+    t_out = format_time(res.time)
+    m_out = format_mem(res.memory)
+
+
+    day = split(split(f, ".")[1], "-")[2]
+    outputs[parse(Int, day)] = "| " * day * " | " * t_out * " | " * m_out * " |\n"
+  end
+
+  isfile("benchmarks.md") && rm("benchmarks.md")
+
+  open("benchmarks.md", "w") do io
+    write(io, "| Day 📅| Time⏳ | Memory 💾 |\n")
+    write(io, "| - | - | - |\n")
+
+    for (_, output) in outputs
+      write(io, output)
     end
+    write(io, "\n")
   end
 
 
+else
+  for a in ARGS
+    for f in files
+      if a == split(split(f, ".")[1], "-")[2]
+        @suppress begin
+          include(dirname(@__DIR__) * "/aoc21/src/" * f)
+          using .Day
+        end
+
+        benchmark = @benchmarkable Day.main()
+        tune!(benchmark)
+
+        res = median(run(benchmark))
+
+        t_out = format_time(res.time)
+        m_out = format_mem(res.memory)
+        day = split(split(f, ".")[1], "-")[2]
+
+        println("Day $(day):\t$(t_out)\t$(m_out)")
+      end
+    end
+  end
 end
-
-
