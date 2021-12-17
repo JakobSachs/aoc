3439a4417551d2af3a058f99dcc1b558ec935534
Jakob Sachs <jakobsachs1999@gmail.com>
2021-12-17 13:12:20 +0100

    Perfomance increases by limiting the simulation domain

--- src/advent-17.jl
+++ src/advent-17.jl
@@ -40,32 +40,35 @@ function main()
   successfull_vels = Vector[]
 
 
-  for x = -500:500
+  for x = 1:500 # seems like a reasonable range to search in
     for y = -500:500
       local_max = 0
 
       probe = [0, 0, x, y]
-      for _ = 1:1000 # limit iteration to 1000
+      while true
         if probe[2] > local_max
           local_max = probe[2]
         end
         if check_target(probe, area)
           push!(successfull_vels, [x, y])
-          if local_max >  max_height
+          if local_max > max_height
             global max_height = local_max
           end
           break
+        elseif probe[1] > area[2] || (probe[2] < area[4] && probe[4] < 0) || (probe[2] > area[3] && probe[4] > 0)
+          break
         end
+
         step!(probe)
       end
     end
   end
 
-  
+
   global successfull_velocities = length(successfull_vels)
 end
 
-function output() 
+function output()
   println("max height:\t\t$(max_height)")
   println("unique velocities:\t$(successfull_velocities)")
 end
